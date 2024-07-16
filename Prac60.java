242bdd79f6a461f238675c2606200276008299c3
Aayush sojitra <142959829+Aayush-sojitra@users.noreply.github.com>
2024-07-16 13:11:23 +0530

    DS

--- Prac60.java
+++ Prac60.java
@@ -50,12 +50,38 @@ public class Prac60 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         LinkedListDemo l1=new LinkedListDemo();
-        l1.push(25);
-        l1.push(12);
-        l1.push(36);
-        l1.display();
-        l1.pop();
-        l1.pop();
-        l1.display();
+        System.out.println("which method want you call");
+        System.out.println("for push 1, for pop 2, for display 3");
+
+        System.out.println("enter no for call method");
+        int m = sc.nextInt();
+
+        while(true){
+            switch (m) {
+                case 1:
+                    System.out.println("enter element to add");
+                    int p = sc.nextInt();
+                    l1.push(p);
+                    break;
+                case 2: System.out.println("delete element from stack");
+                        int x = l1.pop();
+                        System.out.println(x+" is remove from stack");
+                    
+                    break;
+                
+                case 3: System.out.println("diplay all element of stack");
+                        l1.display();
+                    
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("Not valid Number");
+                    break;
+                }
+                System.out.println("Enter");
+                m=sc.nextInt();
+        }
+                
     }
 }
